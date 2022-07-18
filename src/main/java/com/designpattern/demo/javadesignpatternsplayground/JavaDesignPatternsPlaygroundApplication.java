63df7a159fda5e5fdb77e1ab50daa092ef004ee6
nhut_pham <nhut.pham@ilogic.vn>
2022-07-18 08:47:10 +0700

    init proj

---
+++ src/main/java/com/designpattern/demo/javadesignpatternsplayground/JavaDesignPatternsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.designpattern.demo.javadesignpatternsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPatternsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignPatternsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designpattern/demo/javadesignpatternsplayground/JavaDesignPatternsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpattern.demo.javadesignpatternsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignPatternsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
